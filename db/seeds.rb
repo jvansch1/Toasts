f2b77c720a9c4a4872fd40d243575ce4280b0291
John Van Schultz <jvansch1@binghamton.edu>
2017-02-19 21:00:06 -0500

    require faker in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# require 'Faker'
-# I18n.reload!
+require 'faker'
+I18n.reload!
 
 
 Beer.destroy_all
