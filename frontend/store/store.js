218de70ad221715a57c78b1fb84fb3a07fd44efe
John Van Schultz <jvansch1@binghamton.edu>
2017-02-24 00:51:40 -0500

    remove logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import RootReducer from '../reducers/root_reducer';
-import createLogger from 'redux-logger'
+// import createLogger from 'redux-logger'
 
 
 // will need to add preloade state later to keep track of user on window
