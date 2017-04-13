0126f68ef616390306ade9acca24e0ca83c8f0af
John Van Schultz <jvansch1@binghamton.edu>
2017-04-13 15:10:18 -0400

    Refactor out top beers into function on brewery show

--- frontend/components/breweries/brewery_show.jsx
+++ frontend/components/breweries/brewery_show.jsx
@@ -43,6 +43,37 @@ class BreweryShow extends React.Component {
     return url
   }
 
+  breweryShowRight() {
+    return (
+      <div id='brewery-show-right'>
+        <h1 id='brewery-beer-header'>Beers by {this.props.brewery.name}</h1>
+        <ul>
+          {
+            this.props.brewery.top_beers.map((beerArray, idx) => {
+            return (
+                <li id='brewery-show-top-beer-item' key={idx}>
+                  <img id='brewery-show-top-beer-image' src={this.renderDefault(beerArray[1])} />
+                  <ul id='brewery-show-top-beer-info'>
+                    <Link to={`beers/${beerArray[0].id}`}>
+                      <li id='top-name'>
+                        {beerArray[0].name}
+                      </li>
+                    </Link>
+                    <Link to={`breweries/${this.props.brewery.id}`}>
+                      <li id='top-brewery-name'>
+                        {this.props.brewery.name}
+                      </li>
+                    </Link>
+                  </ul>
+                </li>
+              )
+          })
+        }
+        </ul>
+      </div>
+    )
+  }
+
   render() {
     if (this.props.brewery === undefined || this.props.brewery.checkins === undefined) return null;
     const ratingLength = `${this.averageRating() * 25}px`
@@ -50,25 +81,18 @@ class BreweryShow extends React.Component {
         <div>
           <HeaderContainer />
           <div id='brewery-page-container'>
-
-
             <div id='brewery-show-left'>
               <section className='breweryShow'>
                 <div id='brewery-name-flex'>
                   <img className='brewery-image' src={this.props.brewery.image_url} />
                   <span className='brewery-show-name'>
-
                     {this.props.brewery.name}
-
                   </span>
                   <span className='brewery-show-location'>
-
                     {this.props.brewery.city}, {this.props.brewery.state}
                   </span>
                 </div>
-
                 <div className='beer-show-star-ratings-css'>
-
                   <div className='beer-show-star-ratings-css-top' style={{width: ratingLength}} >
                     <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
                   </div>
@@ -77,8 +101,6 @@ class BreweryShow extends React.Component {
                     <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
                   </div>
                 </div>
-
-
                 <div className='brewery-checkin-stats'>
                   <span className='checkin-count'>
                     <p className='first'>
@@ -89,16 +111,12 @@ class BreweryShow extends React.Component {
                     </p>
                   </span>
                 </div>
-
-
-
                 <ul id='brewery-stats'>
                   <li>{this.props.brewery.checkins.length} Ratings</li>
                   <li>{this.props.brewery.beers} beers</li>
                   <li>Date Added - {this.props.brewery.created_at} ago</li>
                 </ul>
               </section>
-
               <div id='brewery-checkin-list-wrapper'>
                 {this.props.brewery.checkins.map((checkin) => {
                   return <BreweryCheckinListItem checkin={checkin} brewery={this.props.brewery} key={checkin.id}/>
@@ -106,32 +124,8 @@ class BreweryShow extends React.Component {
               }
             </div>
           </div>
-          <div id='brewery-show-right'>
-            <h1 id='brewery-beer-header'>Beers by {this.props.brewery.name}</h1>
-            <ul>
-              {
-                this.props.brewery.top_beers.map((beerArray, idx) => {
-                return (
-                    <li id='brewery-show-top-beer-item' key={idx}>
-                      <img id='brewery-show-top-beer-image' src={this.renderDefault(beerArray[1])} />
-                      <ul id='brewery-show-top-beer-info'>
-                        <Link to={`beers/${beerArray[0].id}`}>
-                          <li id='top-name'>
-                            {beerArray[0].name}
-                          </li>
-                        </Link>
-                        <Link to={`breweries/${this.props.brewery.id}`}>
-                          <li id='top-brewery-name'>
-                            {this.props.brewery.name}
-                          </li>
-                        </Link>
-                      </ul>
-                    </li>
-                  )
-              })
-            }
-            </ul>
-          </div>
+
+          {this.breweryShowRight()}
 
         </div>
 
