6d955ab83616c78bd12aca8c2836cbd07427428d
John Van Schultz <jvansch1@binghamton.edu>
2017-05-13 17:53:35 -0400

    Add show tests to beer_controller_spec

--- spec/controllers/beers_controller_spec.rb
+++ spec/controllers/beers_controller_spec.rb
@@ -3,11 +3,30 @@ require 'rails_helper'
 
 describe Api::BeersController do
   describe "get #index" do
+    it "renders the index view" do
+      get :index, format: :json
+    end
+
     it "returns all beers" do
       brewery = Brewery.create(name: 'abcd', city: 'New York', state: 'NY', country: 'USA')
       beer = Beer.create!(name: 'abc', style: 'Bitter', brewery: brewery, ABV: 5.6, IBU: 70)
       get :index, format: :json
       expect(assigns(:beers)).to eq([beer])
+    end
+  end
+
+  describe "get #show" do
+    it "renders the show view" do
+      brewery = Brewery.create(name: 'abcd', city: 'New York', state: 'NY', country: 'USA')
+      beer = Beer.create!(name: 'abc', style: 'Bitter', brewery: brewery, ABV: 5.6, IBU: 70)
+      get :show, format: :json, id: beer.id
+    end
+
+    it "returns the correct beer" do
+      brewery = Brewery.create(name: 'abcd', city: 'New York', state: 'NY', country: 'USA')
+      beer = Beer.create!(name: 'abc', style: 'Bitter', brewery: brewery, ABV: 5.6, IBU: 70)
+      get :show, format: :json, id: beer.id
+      expect(assigns(:beer)).to eq(beer)
     end
   end
 end
