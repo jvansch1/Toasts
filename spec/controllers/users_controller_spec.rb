2e43e86a6f0e7bf7ed9f79e9dae88d044d9b3cfd
John Van Schultz <jvansch1@binghamton.edu>
2017-05-13 17:29:15 -0400

    Add POST method tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -17,7 +17,6 @@ describe Api::UsersController do
     end
 
     describe "GET #show" do
-
       it "renders the :show view" do
         user = User.create(username: 'abcd', password: '456789')
         get :show, format: :json, id: user.id
@@ -27,8 +26,20 @@ describe Api::UsersController do
         user = User.create(username: 'abcd', password: '456789')
         get :show, format: :json, id: user.id
         expect(assigns(:user)).to eq(user)
+      end
+    end
 
+    describe "post #create" do
+      it "with valid attributes" do
+        user = User.create(username: 'abcd', password: '456789')
+        post :create, user: { user: user }
+        expect(User.all.length).to eq(1)
+      end
 
+      it "with invalid attributes" do
+        user = User.create(username: 'abcd', password: '4')
+        post :create, user: { user: user }, format: :json
+        expect(response.body).to eq("[\"Username can't be blank\",\"Password digest can't be blank\"]")
       end
     end
 
