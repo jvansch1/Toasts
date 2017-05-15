e502152e5f59c65884909559d92d5937c3454a54
John Van Schultz <jvansch1@binghamton.edu>
2017-05-15 15:21:04 -0400

    Add tests to validate user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,22 @@ describe User do
   describe "checks validations" do
     it "has a username" do
       user = User.new(username: '', password: 'password')
+      expect(user).to be_invalid
+    end
+
+    it "has a password" do
+      user = User.new(username: 'abcdef', password: '')
+      expect(user). to be_invalid
+    end
+
+    it "password is 6 characters" do
+      user = User.new(username: 'username', password: 'pass')
+      expect(user).to be_invalid
+    end
+
+    it "create user when valid" do
+      user = User.create!(username: 'abcdef', password: 'password')
+      expect(User.count).to eq(1)
     end
   end
 end
